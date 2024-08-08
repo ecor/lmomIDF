b7337580d473c8eb896ecb8f865afd66f1008c21
Emanuele Cordano <emanuele.cordano@gmail.com>
2024-08-08 09:09:46 +0200

    updates

--- inst/scripts.R/build_website.R
+++ inst/scripts.R/build_website.R
@@ -3,5 +3,3 @@ library(pkgdown)
 destination_site="/home/ecor/local/rpackages/jrc/lmomIDF_site"
 pkg ="/home/ecor/local/rpackages/jrc/lmomIDF"
 build_site(pkg=pkg,override = list(destination = destination_site))
-
-####
